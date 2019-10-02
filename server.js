d336528245ed86884d1a626cbee62515572a2f7b
teddyhn <tngo97@gmail.com>
2019-10-02 12:11:48 -0700

    GET request to /api/users functional

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter');
 const postRouter = require('./posts/postRouter');
 
 const server = express();
 
 server.use(logger);
+
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,4 @@
-const express = 'express';
+const express = require('express');
 
 const Users = require('./userDb');
 
@@ -13,7 +13,14 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+    Users.get()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "error retrieving users" })
+        })
 });
 
 router.get('/:id', (req, res) => {
