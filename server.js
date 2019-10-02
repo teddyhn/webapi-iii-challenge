c18a351c40105ea1fe9a02d3f7e6d66c1e3f5b96
teddyhn <tngo97@gmail.com>
2019-10-02 11:48:48 -0700

    Added custom logger middleware

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const postRouter = require('./posts/postRouter');
+
 const server = express();
+
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +13,8 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`Method: ${req.method}, URL: ${req.url}, Timestamp: ${new Date()}`);
+  next();
 };
 
 module.exports = server;
